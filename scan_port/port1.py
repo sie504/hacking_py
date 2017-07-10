a0257e540571f9ced83bae710ced7a17b505af93
sie504 <python2017@sina.com>
2017-07-10 15:48:53 +0800

    a py of scan of port

---
+++ scan_port/port1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+import socket
+import time
+
+def scan(ip,port):
+    try:
+        socket.setdefaulttimeout(3)
+        s = socket.socket()
+        s.connect((ip,port))
+        return True
+    except:
+        return
+
+def scanport():
+    domain = raw_input("Please input your target:")
+    ip = socket.gethostbyname(domain)
+    print 'ip: %s' % ip
+    portlist = [80,8080,9090,3306,3389]
+    starttime = time.time()
+    for port in portlist:
+        res = scan(ip,port)
+        if res:
+            print 'this port:%s is on' % port
+    endtime = time.time()
+    print "This scan spent %s" %(endtime - starttime)
+
+if __name__ == '__main__':
+    scanport()
+
+
+
+
+
+
+
+
+
