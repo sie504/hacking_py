d8c1b8edaf532dccc3e0ac31258c0a1c17f1432e
sie504 <python2017@sina.com>
2017-07-17 16:03:11 +0800

    Blasting

---
+++ Blasting/zip/zip.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import zipfile
+import threading
+
+def zipbp(zfile,pwd):
+    try:
+        zfile.extractall(pwd=pwd)
+        print 'password found : %s' % pwd
+    except:
+        return
+
+def main():
+    zfile = zipfile.ZipFile('test.zip')
+    pwdall = open('dict.txt')
+    for pwd in pwdall.readlines():
+        pwd = pwd.strip('\n')
+        t = threading.Thread(target=zipbp,args=(zfile,pwd))
+        t.start()
+
+if __name__ == '__main__':
+    main()
+
